7c0c2259deb1203bd781adb2494233bb74d82714
fynnbehnke <fynn.behnke@gmail.com>
2024-01-11 11:44:25 +0100

    edit code for workshop

--- robotics_day/src/p_controller.py
+++ robotics_day/src/p_controller.py
@@ -14,8 +14,10 @@ pose_th = 0
 def odom_callback(odom):
 
     global pose_x, pose_y, pose_th
-    pose_x = odom.pose.pose.position.x
-    pose_y = odom.pose.pose.position.y
+    
+    # siehe Bildschirm, um die Roboterpose zu setzen
+
+
 
     q = (
         odom.pose.pose.orientation.x,
@@ -23,8 +25,9 @@ def odom_callback(odom):
         odom.pose.pose.orientation.z,
         odom.pose.pose.orientation.w
     )
+
     roll, pitch, yaw = euler_from_quaternion(q)
-    pose_th = yaw
+
 
 
 
@@ -36,26 +39,27 @@ if __name__ == '__main__':
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     sub = rospy.Subscriber("/odom", Odometry, odom_callback, queue_size=10)    
 
-    posen = [[1,1,math.radians(45)],[2,2,math.radians(45)],[3,3,math.radians(0)]]
+    # posen = [[1,1,math.radians(45)],[2,2,math.radians(45)],[3,3,math.radians(0)]] posen definieren
 
    
     rate = rospy.Rate(10)
 
-    twist_msg = Twist()
+    twist_msg = Twist() 
 
     i = 0
-    Kp = 0.9
-    ka = 1.3
-    kb = -1.2
+
+    # Kp = 0.1
+    # ka = 0.2
+    # kb = -0.3 Parameter selbstständig definieren
 
     while not rospy.is_shutdown():  
         
-        
-        distance_to_goal = math.sqrt(math.pow((posen[i][0] - pose_x), 2) + math.pow((posen[i][0] - pose_y), 2))
+    # berechne den Abstand zwischen Roboterpose und Zielpose     
+    
+    
+    # berechne alpha (winkel zum Ziel)
 
-        alpha = math.atan2(posen[i][1] - pose_y, posen[i][0] - pose_x) - pose_th
-
-        beta = posen[i][2] - alpha -pose_th
+    # berechne beta (Endorientierung)
 
         if(alpha > math.pi):
     
@@ -72,20 +76,30 @@ if __name__ == '__main__':
         if(beta < - math.pi):
             beta = beta + 2*math.pi
 
-        if distance_to_goal < 0.01: #0.1
+
+        # setze einen Toleranzwert, P-Regler hat immer eine Abweichung !!
+            
+        
+
+        if distance_to_goal < tolerance: #0.1
             
             twist_msg.linear.x = 0
             twist_msg.angular.z = 0
             pub.publish(twist_msg)
+            
             i = i+1
 
-            if i > 2:
-                rospy.loginfo("Distance is smaller than 0.1. Shutting down the node.")
-                rospy.signal_shutdown("Distance is smaller than 0.1")
+            if i > len(posen)-1: 
+                
+                rospy.signal_shutdown("") # system wird gekillt, schreibe einen vernünftigen Kommentar
         
 
-        v = Kp*distance_to_goal
-        w = ka*alpha + kb*beta
+        # linear velocity berechne
+        # angular velocity
+    
+
+
+        # setze Beschränkungen der Geschwindigkeiten 
 
         if v > 0.2:
             v = 0.2
@@ -94,12 +108,12 @@ if __name__ == '__main__':
         if w > 1:
             w = 1
 
+       
         twist_msg.linear.x = v  # Example linear velocity
         twist_msg.angular.z = w  # Example angular velocity
     
         # Publish the Twist message
         pub.publish(twist_msg)
-        print("Twist message published!")
-        print(twist_msg.linear.x)
-        print(twist_msg.angular.z)
+        
+
         rate.sleep()
